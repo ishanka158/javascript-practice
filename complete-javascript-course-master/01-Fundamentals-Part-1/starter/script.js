9a95207db7ee9e01dfaf8bc3e0594dd31ebf1b15
ishanka158 <96714@fhss.sjp.ac.lk>
2023-09-14 22:21:23 +0530

    first part finish

---
+++ complete-javascript-course-master/01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,21 @@
+/*
+let js = "amazing"
+
+console.log(40 + 8 + 23 - 10);
+
+js = "2"
+console.log(js);
+
+
+const firstName = "Madhuranga";
+const job = "Software engineer";
+
+const birthYear = 1999;
+const year = 2023;
+
+const madhuranga = "I'm " + firstName + ", a " + (year - birthYear) + " old " + job;
+const newMadhuranga = `I'm ${firstName}, a ${(year - birthYear)} old ${job}`
+
+console.log(madhuranga);
+console.log(newMadhuranga);
+*/
